f0c78e2856733e4946101909cd1ef220b6b90122
huba1huba <105104242+huba1huba@users.noreply.github.com>
2022-05-08 22:58:05 +0800

    Update enter.java

--- src/main/java/enter.java
+++ src/main/java/enter.java
@@ -6,6 +6,6 @@ package src.main.java;
  */
 public class enter {
     public static void main(String[] args) {
-        System.out.println("hello,git,master!");
+        System.out.println("hello,git,master,remote!");
     }
 }
