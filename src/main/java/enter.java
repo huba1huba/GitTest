226474078c8c8c5be2794c2e0fdfe3f6b3cf54d5
hxy <hxy@163.com>
2022-05-08 22:29:11 +0800

    hxy fourth commit from idea

--- src/main/java/enter.java
+++ src/main/java/enter.java
@@ -6,6 +6,6 @@ package src.main.java;
  */
 public class enter {
     public static void main(String[] args) {
-        System.out.println("hello,git!");
+        System.out.println("hello,git,master!");
     }
 }
